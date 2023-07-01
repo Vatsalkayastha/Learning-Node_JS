baab91b1f34fa319c27cf757c0250fcd80720f1b
Vatsalkayastha <vatsal.k2@ahduni.edu.in>
2023-07-01 16:31:32 +0530

    second commit

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
-const { log } = require('console');
-const _ = require('lodash')
+const http = require('http')
 
-const items = [1,[2,[3,[4]]]]
-const newItems = _.flattenDeep(items);
-console.log(newItems);
+const server = http.createServer((req,res)=>{
+    if(req.url == '/'){
+
+        res.end("Home Page")
+    }
+    if(req.url == '/about'){
+
+        res.end("About Page")
+    }
+    res.end('Error Page')
+    
+})
+
+server.listen(3000,()=>{
+    console.log('Server is listening on port 3000...');
+})
